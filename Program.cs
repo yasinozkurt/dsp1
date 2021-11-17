16511ee6e293bdd6bfcc2697325a286efd8374c4
yasinozkurt <05190000087@ogrenci.ege.edu.tr>
2021-11-17 14:16:37 +0300

    a,b,c yapıldı c ye eklemeler lazım

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DsProject1._1
 {
@@ -15,7 +16,10 @@ namespace DsProject1._1
             //deneme
             //****************************************************************************************************************
             //a kısmı kontrolü -OK
-            MatrixClass d = new MatrixClass(10,100,100); //d nesnesi n width ve height parametreleri verilerek oluşturuldu
+            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            Console.WriteLine("000000000000000000000000        ---A KISMI KONTROL---        0000000000000000000000");
+            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            MatrixClass d = new MatrixClass(20,100,100); //d nesnesi n width ve height parametreleri verilerek oluşturuldu
             double[,]matris=d.randomNumber();   //d nesnesine ait matris oluşturuldu
 
 
@@ -33,6 +37,8 @@ namespace DsProject1._1
 
             //b kısmı kontrolü --OK
             Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            Console.WriteLine("000000000000000000000000        ---B KISMI KONTROL---        0000000000000000000000");
+            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
             double[,] m= d.distanceMatrix();
             for(int i = 0; i < d.matrix.GetLength(0); i++)
             {
@@ -46,6 +52,30 @@ namespace DsProject1._1
                 Console.WriteLine();
                 Console.WriteLine();
             }
+
+
+            //c kısmı kontrolü
+            //
+            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            Console.WriteLine("000000000000000000000000        ---C KISMI KONTROL---        0000000000000000000000");
+            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+            ArrayList a = new ArrayList();
+            double u;
+            (a,u)=d.nNeighbor();
+
+            Console.WriteLine("uzaklık: " + u);
+            for(int i = 0; i < matris.GetLength(0); i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+
+
+
+
+
+
+
             //denemebitiş
             //****************************************************************************************************************
 
@@ -124,6 +154,86 @@ namespace DsProject1._1
 
             return dM;
 
+        }
+
+
+
+
+
+
+        //nearest neighbor metodu
+
+
+
+
+        public (ArrayList,double) nNeighbor()
+        {
+            double toplamUzaklık = 0;
+
+            // gezilen noktaları basitçe eklemek için arraylist kullanıyorum
+            ArrayList gezilenNoktalar = new ArrayList();
+            //en yakın nokta sıralaması:
+            ArrayList nearestPointList = new ArrayList();
+
+            //başlangıç için ilk random noktayı seçiyorum:
+            Random rnd = new Random();
+
+
+            int count = 0;// aşağıdaki for döngüsü içinde min sayıyı bulabilmek için
+
+            int nn = rnd.Next(0, matrix.GetLength(0));
+
+            double tempMinDist = 0;
+
+            int nnIndex=0;
+         
+           
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+               
+                gezilenNoktalar.Add(nn);  
+
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    if ((i == j)|gezilenNoktalar.Contains(j))
+                    {
+                        //i==j durumu noktanın kendisine uzaklığı vereceğinden anlamsızdır
+                        continue;
+                    }
+                    count++;
+                    if (count == 1)//min sayıyı bulmak için pivot değer ataması
+                    {
+                        tempMinDist = uzaklıkHesapla(matrix[nn, 0], matrix[j, 0], matrix[nn, 1], matrix[j, 1]);
+                        nnIndex = j;
+                    }
+
+                    if(uzaklıkHesapla(matrix[nn, 0], matrix[j, 0], matrix[nn, 1], matrix[j, 1]) < tempMinDist)  //for döngüsü boyunca döndürüp gerçek min değer bulunuyor
+                    {
+                        tempMinDist = uzaklıkHesapla(matrix[nn, 0], matrix[j, 0], matrix[nn, 1], matrix[j, 1]);
+                        nnIndex = j;
+
+                    }
+
+
+                }
+                nn = nnIndex;
+               
+                toplamUzaklık += tempMinDist;
+
+
+                count = 0;
+            }
+
+
+            //matematiksel hesplama için lokal metod:
+            double uzaklıkHesapla(double x1, double x2, double y1, double y2)
+            {
+                double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+                return distance;
+            }
+
+            return (gezilenNoktalar,toplamUzaklık);
         }
 
     }
